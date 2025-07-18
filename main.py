05817bad151cee71d9efd23b3ffb477bfa54ed78
MKDBGNS <154989220+MKDBGNS@users.noreply.github.com>
2025-07-18 23:23:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from pyrogram import Client
 from pytgcalls import idle
 from callsmusic import run
 from config import API_ID, API_HASH, STRING_SESSION
+import pytgcalls
+print("✅ PyTgCalls version:", getattr(pytgcalls, "__version__", "unknown"))
 
 userbot = Client(STRING_SESSION, API_ID, API_HASH)
 userbot.start()
