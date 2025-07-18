14d00bdbb56690e86749077e17c4f0de424846fe
MKDBGNS <154989220+MKDBGNS@users.noreply.github.com>
2025-07-18 21:37:37 +0530

    Update callsmusic.py

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -3,7 +3,6 @@ from pyrogram import Client
 from pytgcalls import PyTgCalls
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped
-from pytgcalls.types.input_stream import InputStream
 from queues import queues
 
 client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
@@ -18,14 +17,10 @@ async def on_stream_end(client: PyTgCalls, update: Update) -> None:
     if queues.is_empty(chat_id):
         await pytgcalls.leave_group_call(chat_id)
     else:
-        await pytgcalls.change_stream(
-            chat_id, 
-            InputStream(
-                InputAudioStream(
-                    queues.get(chat_id)["file"],
-                ),
-            ),
-        )
+      await pytgcalls.change_stream(
+    chat_id,
+    AudioPiped(queues.get(chat_id)["file"])
+)
 
 
 run = pytgcalls.start
